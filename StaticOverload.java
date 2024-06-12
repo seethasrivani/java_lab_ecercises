a5a0153ba6dc14efdffa10bcd601df322560a6b0
seethasrivani <seethasrivani123@gmail.com>
2024-06-12 15:42:08 +0530

    Add files via upload

---
+++ StaticOverload.java
@@ -0,0 +1,37 @@
+package polimarphism;
+class Operations
+{
+	public void add(int a)
+	{
+		a++;
+		System.out.println(a);
+	}
+	public int add(float a)
+	{
+		a++;
+		return (int)a;
+	}
+	public void concat(char c,int a)
+	{
+		System.out.println(c+a);
+	}
+	public void concat(int a,char c)
+	{
+		System.out.println(c+a);
+	}
+}
+
+public class StaticOverload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Operations op=new Operations();
+		op.add(9);
+		System.out.println(op.add(8.9f));
+		op.concat('a', 9);
+		op.concat(8,'j');
+
+	}
+
+}
